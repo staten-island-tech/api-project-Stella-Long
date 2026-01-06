8215b0122e004b0a8dbcf33008a3faff38ee3be9
Stella-Long <huierl@nycstudents.net>
2026-01-06 12:32:11 -0500

    s

--- api-project/src/main.js
+++ api-project/src/main.js
@@ -1,24 +1,75 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+let data = [
+  {
+    id: "",
+    name: "",
+    alternate_names: "",
+    species: "",
+    gender: "",
+    house: "",
+    dateOfBirth: "",
+    yearOfBirth: "",
+    wizard: "",
+    ancestry: "",
+    eyeColour: "",
+    hairColour: "",
+    wand: {
+      wood: "",
+      core: "",
+      length: "",
+    },
+    patronus: "",
+    hogwartsStudent: "",
+    hogwartsStaff: "",
+    actor: "",
+    alternate_actors: "",
+    alive: "",
+    image: "",
+  },
+];
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const apiUrl = "https://hp-api.onrender.com/api/characters";
 
-setupCounter(document.querySelector('#counter'))
+async function getData(apiURL) {
+  try {
+    const response = await fetch(apiURL);
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      const characters = data;
+      const container = document.querySelector(".container");
+      characters.forEach((character) => {
+        container.insertAdjacentHTML(
+          "beforeend",
+          `
+          <div class = "cards">
+          <h2>id: ${character.id}</h2>
+          <h2>name: ${character.name}</h2>
+          <h2>alternate_names: ${character.alternate_names}</h2>
+          <h2>species: ${character.species}</h2>
+          <h2>gender: ${character.gender}</h2>
+          <h2>house: ${character.house}</h2>
+          <h2>dateOfBirth: ${character.dateOfBirth}</h2>
+          <h2>yearOfBirth: ${character.yearOfBirth}</h2>
+          <h2>wizard: ${character.wizard}</h2>
+          <h2>ancestry: ${character.ancestry}</h2>
+          <h2>eyeColor: ${character.eyeColour}</h2>
+          <h2>hairColor: ${character.hairColour}</h2>
+          <h2>wand: ${character.wand}</h2>
+          <h2>patronus: ${character.patronus}</h2>
+          <h2>hogwartsStudent: ${character.hogwartsStudent}</h2>
+          <h2>hogwartsStaff: ${character.hogwartsStaff}</h2>
+          <h2>actor: ${character.actor}</h2>
+          <h2>alterante_actor: ${character.alternate_actors}</h2>
+          <h2>alive: ${character.alive}</h2>
+          <h2>image: ${character.image}</h2>
+          </div>
+        `
+        );
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(apiUrl);
