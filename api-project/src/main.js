5946bd6ed518706b378e97fb8b0d251dc740483a
Stella-Long <huierl@nycstudents.net>
2026-01-12 11:52:52 -0500

    s

--- api-project/src/main.js
+++ api-project/src/main.js
@@ -2,11 +2,10 @@ let data = [
   {
     id: "",
     name: "",
-    effect: "",
   },
 ];
 
-const apiURL = "https://wizard-world-api.herokuapp.com/Ingredients";
+const apiUrl = "https://wizard-world-api.herokuapp.com/Ingredients";
 
 async function getData(apiURL) {
   try {
@@ -15,36 +14,35 @@ async function getData(apiURL) {
       throw new Error(response);
     } else {
       const data = await response.json();
-      const spells = data;
+      const ingredients = data;
       const container = document.querySelector(".container");
-      function showSpells(list) {
+      function showIngredients(list) {
         container.innerHTML = "";
-        list.forEach((spell) => {
+        list.forEach((ingredient) => {
           container.insertAdjacentHTML(
             "afterbegin",
             `
               <div id = "cards" class = " m-8 p-8 border-4 border-sky-900 bg-gray-200">
-              <div id="name" class ="font-bold underline underline-offset-4">${spell.name}</div>
-              <div id="id">Id: ${spell.id}</div>
+                <div id="name" class ="font-bold underline underline-offset-4">${ingredient.name}</div>
+              </div>    
             `
           );
         });
       }
-      showSpells(spells);
+      showIngredients(ingredients);
       function filter(input) {
         const search = input.toLowerCase();
         container.innerHTML = "";
-        spells.forEach((spell) => {
-          const searchName = spell.name.toLowerCase();
+        ingredients.forEach((ingredient) => {
+          const searchName = ingredient.name.toLowerCase();
           if (searchName.includes(search)) {
             container.insertAdjacentHTML(
               "afterbegin",
               `
-                            <div id = "cards" class = " m-8 p-8 border-4 border-sky-900 bg-gray-200">
-                                <div id="name" class ="font-bold underline underline-offset-4">${spell.name}</div>
-                                <div id="id">Id: ${spell.id}</div>
-                            </div>    
-                            `
+                <div id = "cards" class = " m-8 p-8 border-4 border-sky-900 bg-gray-200">
+                  <div id="name" class ="font-bold underline underline-offset-4">${ingredient.name}</div>
+                </div>    
+              `
             );
           }
         });
